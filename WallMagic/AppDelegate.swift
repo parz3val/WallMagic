a738b2afad6ee091a02e1beed8f116d2a4e98b74
Harris Kunwar <harriskunwar@xxXXxx.local>
2022-07-06 18:38:36 +0545

    do : get the repo out of experiments folder

---
+++ WallMagic/AppDelegate.swift
@@ -0,0 +1,35 @@
+//
+//  AppDelegate.swift
+//  WallMagic
+//
+//  Created by Harris Kunwar on 7/4/22.
+//
+//
+
+import Cocoa
+
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet var window: NSWindow!
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+    }
+
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+    }
+
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+    return true
+    }
+
+
+
+}
